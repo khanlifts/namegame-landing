cd2763162e2b99a02fc7d65d900d3feecf037858
Cyril Khan <24543034+khanlifts@users.noreply.github.com>
2022-11-13 15:00:47 +0100

    add main page header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NamegameHeaderComponent } from './namegame-header/namegame-header.component';
+import { NamegameMainPageComponent } from './namegame-main-page/namegame-main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NamegameHeaderComponent,
+    NamegameMainPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/namegame-main-page/namegame-main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-namegame-main-page',
+  templateUrl: './namegame-main-page.component.html',
+  styleUrls: ['./namegame-main-page.component.sass']
+})
+export class NamegameMainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
