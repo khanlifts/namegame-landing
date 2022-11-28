cedbf8a61c6102f235d12ee2b955fdbcf17d4226
Cyril Khan <24543034+khanlifts@users.noreply.github.com>
2022-11-28 08:10:02 +0100

    add correct min-height on form submit and smooth scroll

--- src/app/namegame-main-page/namegame-main-page.component.ts
+++ src/app/namegame-main-page/namegame-main-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-namegame-main-page',
   templateUrl: './namegame-main-page.component.html',
   styleUrls: ['./namegame-main-page.component.sass']
 })
-export class NamegameMainPageComponent {}
+export class NamegameMainPageComponent implements OnInit {
+  private scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  ngOnInit() {
+    this.scrollToTop();
+  }
+}
--- src/app/namegame-more-info-page/namegame-more-info.component.ts
+++ src/app/namegame-more-info-page/namegame-more-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
@@ -6,7 +6,7 @@ import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
   templateUrl: './namegame-more-info.component.html',
   styleUrls: ['./namegame-main-page.component.sass']
 })
-export class NamegameMoreInfoComponent {
+export class NamegameMoreInfoComponent implements OnInit {
   submittedEmail = '';
   serviceId = 'service_uhe70ub';
   templateId = 'template_6vfanrt';
@@ -14,12 +14,25 @@ export class NamegameMoreInfoComponent {
 
   public sendEmail(submittedEmail: string) {
     this.submittedEmail = submittedEmail;
-    emailjs.send(
-      this.serviceId, this.templateId, { email: submittedEmail }, this.publicKey)
-      .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
-      }, (error) => {
-        console.error(error.text);
-      });
+    this.scrollToTop();
+    // emailjs.send(
+    //   this.serviceId, this.templateId, { email: submittedEmail }, this.publicKey)
+    //   .then((result: EmailJSResponseStatus) => {
+    //     console.log(result.text);
+    //   }, (error) => {
+    //     console.error(error.text);
+    //   });
+  }
+
+  private scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  ngOnInit() {
+    this.scrollToTop();
   }
 }
